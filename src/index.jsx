00ede2e319f2aec4865cc893d7c93a79264e33a1
sadness_ojisan <sadness.ojisan@gmail.com>
2021-07-16 18:25:15 +0900

    Update

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,36 @@
-import {render} from 'react-dom';
+import { render } from "react-dom";
+
+const f = () => 3;
+
+let x = do {
+  let tmp = f();
+  tmp * tmp + 1;
+};
+
+console.log(x);
+
+const Component = (props) => (
+  <div className="myComponent">
+    {do {
+      if (props.color === "blue") {
+        <div style={{ color: "blue" }}>blue </div>;
+      } else if (props.color === "red") {
+        <div style={{ color: "red" }}>red </div>;
+      } else if (props.color === "blue") {
+        <div style={{ color: "blue" }}>blue </div>;
+      } else {
+        // assertNever とかができる！
+      }
+    }}
+  </div>
+);
 
 const Root = () => {
-    return <div>hello</div>
-}
+  return (
+    <div>
+      <Component color="red" />
+    </div>
+  );
+};
 
-render(<Root />, document.getElementById('root'));
+render(<Root />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: "production",
+  mode: "development",
 
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: "./src/index.jsx",
